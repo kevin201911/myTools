1b030aefb4e0fbfad0b6a98c97ddfcb520150289
AloofJr <wq105907@alibaba-inc.com>
2019-10-16 00:34:19 +0800

    feat: add lt

--- src/main/java/com/my/tools/Main.java
+++ src/main/java/com/my/tools/Main.java
@@ -1,27 +1,10 @@
 package com.my.tools;
 
-
-
-import com.alibaba.common.lang.StringUtil;
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import org.apache.commons.httpclient.*;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.httpclient.params.HttpConnectionManagerParams;
-import org.apache.commons.httpclient.params.HttpMethodParams;
-
-import javax.tools.JavaCompiler;
 import java.util.*;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.locks.ReentrantLock;
 
-/**
- * @author wq
- * @date 2018/11/5
- */
-public class Main {
+public class Main{
     public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.clear();
     }
 }
-
